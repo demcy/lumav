8c0d835effdfc7c7be1b9912e0bff60e9eec6b9f
Aleksandr Khokhlachev <demcy.meizu@gmail.com>
2021-04-15 10:20:35 +0300

    clear commit

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+const App = () => {
+    return (
+        <main role="main" className="container">
+
+        </main>
+    )
+};
+
+export default App;
